2003439f054d44612b223276e7ee94da664a1b47
helicon.ho@outlook.com <helicon.ho@outlook.com>
2022-04-14 00:15:56 +0800

    need to set product table

--- src/component/loading.js
+++ src/component/loading.js
@@ -1,18 +1,10 @@
-import { useEffect, useState } from "react";
 import { Spinner } from "react-bootstrap";
 
 function Loading() {
-    const [counter, setCounter] = useState(0);
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCounter(counter + 1);
-        }, 1000);
-        return () => clearInterval(interval);
-    }, [counter]);
     return (
-        <div className="loading-container">
-            {counter > 15 ? <div> </div> : <Spinner animation="border" variant="primary" />}
-        </div>
+        <Spinner animation="border" variant="primary">
+            <span className="sr-only">Loading...</span>
+        </Spinner>
     )
 }
 
