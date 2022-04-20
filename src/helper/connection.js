5872d7476a1e4d5f268fc15e1ef8a0bac6c5df60
helicon.ho@outlook.com <helicon.ho@outlook.com>
2022-04-21 00:27:13 +0800

    backup

--- src/helper/connection.js
+++ src/helper/connection.js
@@ -1,4 +1,4 @@
 import * as Realm from "realm-web";
 const appKey = process.env.REACT_APP_KEY;
-const app = new Realm.App({ id: appKey });
+const app = new Realm.App({ id: "cms-chclo" });
 export { app };
