d7571c9729010e67bfb3f808f9e62ef8798481e6
helicon.ho@outlook.com <helicon.ho@outlook.com>
2022-04-20 23:36:16 +0800

    Updates

--- src/helper/connection.js
+++
@@ -1,4 +0,0 @@
-import * as Realm from "realm-web";
-const appKey = process.env.REACT_APP_KEY;
-const app = new Realm.App({ id: appKey });
-export { app };
--- src/pages/User.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const UserPage = () =>{
-  return(
-    <h1>UserPage</h1>
-  );
-}
-export default UserPage;
